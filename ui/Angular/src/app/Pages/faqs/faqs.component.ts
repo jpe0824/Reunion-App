0b2463ebfbb87465b0b786ca3e344063da66b320
jpe0824 <jason.e24@gmail.com>
2023-01-01 14:12:55 -0700

    added all faq's

--- ui/Angular/src/app/Pages/faqs/faqs.component.ts
+++ ui/Angular/src/app/Pages/faqs/faqs.component.ts
@@ -6,6 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./faqs.component.scss']
 })
 export class FaqsComponent implements OnInit {
+  public faq1isCollapsed = true;
+  public faq2isCollapsed = true;
+  public faq3isCollapsed = true;
+  public faq4isCollapsed = true;
+  public faq5isCollapsed = true;
+  public faq6isCollapsed = true;
+  public faq7isCollapsed = true;
+  public faq8isCollapsed = true;
+  public faq9isCollapsed = true;
+  public faq10isCollapsed = true;
 
   constructor() { }
 
